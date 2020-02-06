b5b38c52144416bc18f18cb1fa2fc40486f0404d
Alberto Resco Perez <alberto.resco@prudential.com.sg>
2020-02-06 16:47:50 +0800

    Add import

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -17,7 +17,10 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.client.RestTemplate;
+
 
 @RestController
 public class Controller {
